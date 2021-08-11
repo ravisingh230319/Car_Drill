a0357aa0c6f248077f2f62d1d10db429d8c4a342
Ravi Kumar Singh <ravi.singh.17@mountblue.tech>
2021-08-11 16:08:20 +0530

    Updated problem3.js

--- problem3.js
+++ problem3.js
@@ -2,7 +2,7 @@ import { inventory } from './inventory.js';
 let carModel=[];
 for(let i=0;i<inventory.length;i++)
     {
-       carModel[i]=inventory[i].car_model;
+       carModel[i]=inventory[i].car_model.toLowerCase();// Converting to lowercase so that sorting works fine for both lowercase and uppercase.
     }
 export function problem3(inventory){
    carModel.sort();
