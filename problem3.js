ee3e602a5f518c0adadf6c4b54305934904272ff
Ravi Kumar Singh <ravi.singh.17@mountblue.tech>
2021-08-13 01:42:29 +0530

    Updated problem3.js

--- problem3.js
+++ problem3.js
@@ -1,14 +1,28 @@
 import { inventory } from './inventory.js';
 
-export function problem3(inventory){
-let carModel=[];
-for(let i=0;i<inventory.length;i++)
-    {
-       carModel[i]=inventory[i].car_model.toLowerCase();// Converting to lowercase so that sorting works fine for both lowercase and uppercase.
-    }
-   carModel.sort();
-   if(carModel.length==0)
-   return [];
-   return carModel;
-}
 
+export function problem3(inventory)
+{
+   let carModel=[];
+   let pass=false;
+      for(let i=0;i<inventory.length;i++)
+      {
+         carModel[i]=inventory[i].car_model;
+      }
+   
+      for(;!pass;) {
+         pass = true;
+         for (var i = 1; i<carModel.length; i++) {
+            if (carModel[i - 1].toLowerCase()>carModel[i].toLowerCase()) {
+               pass = false;
+               var tmp =carModel[i - 1];
+               carModel[i - 1] = carModel[i];
+               carModel[i] = tmp;
+            }
+         }
+      }
+      if(carModel.length==0)
+      return [];  
+      else 
+      return carModel;   
+   }
